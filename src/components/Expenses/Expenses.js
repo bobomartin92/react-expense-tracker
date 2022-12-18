71acd101fbcef80382a056afdc14e7ce0ebbfe47
Bodiseowei Martin Tonweriyai <bmtonweriyai@gmail.com>
2022-12-18 14:26:56 +0100

    wrap up

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -24,6 +25,7 @@ const Expenses = (props) => {
             selected={filteredYear}
             onChangeFilter={filterChangeHandler}
           />
+          <ExpensesChart expenses={filteredExpenses} />
           <ExpensesList items={filteredExpenses} />
         </Card>
       </div>
